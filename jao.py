1811ee254800170e742b644841d9a35f8f3629b8
Zengor <zengor@hotmail.com>
2017-09-09 20:50:55 -0300

    Do not respond to commands sent before bot activation

--- jao.py
+++ jao.py
@@ -64,7 +64,8 @@ def main():
     dp.add_error_handler(error)
     
     #start bot activity
-    updater.start_polling()
+    #clean means we ignore updates sent while the bot was off
+    updater.start_polling(clean=True)
     updater.idle()
 
 def restricted(func):
